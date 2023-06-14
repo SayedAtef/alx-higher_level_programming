084595cada30cb5194cba4eda14378ac35988009
sayed atef <sayedatef6293@gmail.com>
2023-06-14 18:09:00 +0000

    6

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def print_sorted_dictionary(a_dictionary):
+    listo_of_ord = list(a_dictionary.keys())
+    list_of_ord.sort()
+    for i in list_of_ord:
+        print("{}: {}".format(i, a_dictionary.get(i)))
