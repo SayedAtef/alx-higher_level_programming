06393e465c3f14ed45f90f690b5ad9f45048fe13
sayed atef <sayedatef6293@gmail.com>
2023-07-10 20:14:08 +0000

    0

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-
+"""
+=============================
+Module with the method lookup
+=============================
+"""
 
 def lookup(obj):
     """Function that returns the attributes of a given object"""
