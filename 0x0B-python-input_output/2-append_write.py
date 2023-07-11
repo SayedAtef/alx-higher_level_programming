c87b29cf7e12e902fda892d08eb22f9c88425d77
sayed atef <sayedatef6293@gmail.com>
2023-07-11 20:06:24 +0000

    2

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""append_write module"""
+
+
+def append_write(filename="", text=""):
+    """
+    write_file - appends a string at the end of a text file (UTF8),
+    Args:
+        filename: name of the file
+        text: text to be written
+    Return: number of bytes written.
+    """
+    with open(filename, mode="a", encoding="UTF-8") as f:
+        return (f.write(text))
